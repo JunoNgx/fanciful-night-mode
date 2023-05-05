8eff26d12b0ba66c6b57887c724ff7d9083146bb
Juno Nguyen <juno.ngx@gmail.com>
2023-05-05 14:53:38 +0800

    First commit

---
+++ main.js
@@ -0,0 +1,45 @@
+// import './style.css'
+// import javascriptLogo from './javascript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.js'
+
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//         <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+//         <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+//     </a>
+//     <h1>Hello Vite!</h1>
+//     <div class="card">
+//         <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//         Click on the Vite logo to learn more
+//     </p>
+//   </div>
+// `
+
+// setupCounter(document.querySelector('#counter'))
+
+
+import "./style.sass";
+
+let isDark = false;
+
+const handleClickEvent = () => {
+    isDark = !isDark;
+    updateClasses();
+}
+
+const updateClasses = () => {
+    const darkElements = document.getElementsByClassName("has-dark-mode");
+    for (const elem of darkElements) {
+        elem.classList.toggle("is-dark");
+    };
+}
+
+const mainElem = document.getElementsByClassName("main")[0];
+mainElem.addEventListener("click", handleClickEvent);
+
